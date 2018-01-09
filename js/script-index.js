d1ac8c0791511fc4ca5112c0f1327fcd709e1f0e
Jocelyn1912 <toty_2410@hotmail.com>
2018-01-08 22:39:23 -0300

    Etapa 5: creando la función de la sección de actividades y funcionalidad del mensaje de advertencia si es que no hay actividades

--- js/script-index.js
+++ js/script-index.js
@@ -9,6 +9,9 @@ $(document).ready( function(){
   //La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
   renderHighlightedRecipes(recipesArray);
 
+  //Llamando función renderActivities y pasandole como parametro la variable activitiesArray.
+  renderActivities(activitiesArray);
+
 });
 
 //Función que muestra el texto "nuevas recetas" en la lista de noticias
@@ -22,6 +25,7 @@ function printNews () {
 * marcado el atributo "highlighted" como TRUE
 */
 function renderHighlightedRecipes(recipesArray) {
+  console.log('Recipes: ', recipesArray);
   //Recorriendo el arreglo
   for (var i = 0; i< recipesArray.length; i++) {
     //comprobando condición
@@ -40,22 +44,30 @@ function renderHighlightedRecipes(recipesArray) {
 */
 function renderRecipe(recipe) {
   console.log('Voy a pintar la receta: ', recipe);
-
-    $('.list-recipes').append('<a class="item-recipe" href="#">'+'<span class="attribution">'+'<span class="title-recipe"> Título de la receta: ' + recipe.title + '</span>'+'<span class="metadata-recipe">'+'<span class="author-recipe"> Autor: ' + recipe.source.name + '</span>'+'<span class="bookmarks-recipe">'+'<span class="icon-bookmark">'+'</span>'+'</span>'+'</span>'+'</span>'+'<img src="img/recipes/320x350/' + recipe.name + '.jpg" />'+'</a>');
+  //Creando HTML y mostrando estructura con contenido en el div class=list-recipes
+  $('.list-recipes').append('<a class="item-recipe" href="#">'+'<span class="attribution">'+'<span class="title-recipe"> Título de la receta: ' + recipe.title + '</span>'+'<span class="metadata-recipe">'+'<span class="author-recipe"> Autor: ' + recipe.source.name + '</span>'+'<span class="bookmarks-recipe">'+'<span class="icon-bookmark">'+'</span>'+'</span>'+'</span>'+'</span>'+'<img src="img/recipes/320x350/' + recipe.name + '.jpg" />'+'</a>');
 }
-
-
 
 /*
 * Función que se encarga de pintar todas las actividades
 */
+
+
 function renderActivities(activitiesArray) {
   console.log('Activities: ', activitiesArray);
+  //Recorriendo el array activities
+  for(var i = 0; i < activitiesArray.length; i++) {
+    //Pasando la función el objeto
+    renderActivity(activitiesArray[i]);
+    // Si el arreglo tiene más de cero objetos debe ocultar el div class="wrapper-message"
+  } if (activitiesArray.length > 0){
+    $('.wrapper-message').hide();
+  }
 }
 
 /*
 * Función que se encarga de pintar una actividad
-* Aqui se tiene que crear el HTML que esta en el 
+* Aqui se tiene que crear el HTML que esta en el
 * archivo "templates/templates-activity.html"
 */
 function renderActivity(recipe) {
